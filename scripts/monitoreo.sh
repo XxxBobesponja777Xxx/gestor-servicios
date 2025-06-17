3a5c43289d9730eeb1430dce8d35d90875c2789c
William <william@debian.William>
2025-06-16 21:45:26 -0600

    Actualización del proyecto con nuevas modificaciones

--- scripts/monitoreo.sh
+++ scripts/monitoreo.sh
@@ -2,12 +2,12 @@
 source "/home/william/gestor-servicios/config.txt"
 log_file="$LOG_DIR/monitoreo.log"
 
-cpu=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5); printf "%d", usage}')
+cpu=$(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5); printf "%.0f", usage}')
 disk=$(df / | awk 'END{print +$5}')
 
 message=""
-(( cpu > CPU_THRESHOLD )) && message+="⚠️PU: ${cpu}%\n"
-(( disk > DISK_THRESHOLD )) && message+="⚠️isco: ${disk}%\n"
+(( cpu > CPU_THRESHOLD )) && message+="⚠️CPU: ${cpu}%"
+(( disk >= DISK_THRESHOLD )) && message+=" Disco: ${disk}%"
 
 if [[ -n "$message" ]]; then
   echo "$(date '+%F %T') - $message" >> "$log_file"
--- scripts/tarea_remota.sh
+++ scripts/tarea_remota.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
 echo "Hola desde $(hostname)" >> /tmp/remote_job.log
+echo "Hola desde $(hostname)"
